1097b1fd26f95bbcb96672d9298162bd8a2ff4dc
Michael Roberts <mjr_@hotmail.com>
2017-05-10 04:25:03 +0100

    commonize device interface files (use links in NightMode project); fix TV ON enable checkbox bug

--- PinscapeConfigTool/VersionInfo.cs
+++ PinscapeConfigTool/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace PinscapeConfigTool
 {
     class VersionInfo
     {
-        public static string BuildNumber = "3095"; // #BuildNumber
+        public static string BuildNumber = "3103"; // #BuildNumber
         public static DateTime BuildDate = new DateTime(2000, 1, 1).AddDays(
             System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Build);
     }
