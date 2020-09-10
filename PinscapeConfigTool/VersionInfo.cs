574b2f7658ef222ed87dd168de2e0c71bdb587be
mjrgh <mjr_@hotmail.com>
2020-09-10 20:05:56 +0100

    clean up some html resource 'copy if newer' settings

--- PinscapeConfigTool/VersionInfo.cs
+++ PinscapeConfigTool/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace PinscapeConfigTool
 {
     class VersionInfo
     {
-        public static string BuildNumber = "3935"; // #BuildNumber
+        public static string BuildNumber = "3937"; // #BuildNumber
         public static DateTime BuildDate = new DateTime(2000, 1, 1).AddDays(
             System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Build);
     }
