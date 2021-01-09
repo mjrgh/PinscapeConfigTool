e57a7eb2b0dffd3b057cf04139a491b5a99f3946
Fabrice NLN <60327140+nicofab18@users.noreply.github.com>
2021-01-09 20:37:12 +0100

    Update Program.cs
    version française

--- NightMode/Program.cs
+++ NightMode/Program.cs
@@ -65,20 +65,20 @@ namespace NightMode
             // if we didn't find a unit, return failure
             if (dev == null)
             {
-                MessageBox.Show("The selected unit number (" + unit + ") wasn't found.");
+                MessageBox.Show("Le numéro d'unité sélectionné ("+ unité +") n'a pas été trouvé.");
                 return;
             }
 
             // send the NightMode request
             if (!dev.SpecialRequest(8, new byte[] { (byte)(on ? 1 : 0) }))
-                MessageBox.Show("An error occurred sending the NightMode request to the KL25Z.");
+                MessageBox.Show("Une erreur s'est produite lors de l'envoi de la requête NightMode au KL25Z.");
         }
 
         static void Usage()
         {
             MessageBox.Show("Usage: NightMode <PinscapeUnitNumber> ON|OFF\r\n\r\n"
-                + "For example, \"NightMode 1 ON\" turns on Night Mode on Pinscape unit #1.\r\n\r\n"
-                + "Run without arguments to select interactively.");
+                + "Par exemple, \"NightMode 1 ON\" active le mode nuit sur l'unité Pinscape # 1. \ R \ n \ r \ n "
+                + "Exécuter sans arguments pour sélectionner de manière interactive.");
         } 
     }
 }
