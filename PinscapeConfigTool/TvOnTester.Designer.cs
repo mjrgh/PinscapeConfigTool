33ea83a7d6ba716a2aac179c8a2978505a67cdbc
Fabrice NLN <60327140+nicofab18@users.noreply.github.com>
2021-01-22 19:09:13 +0100

    Update TvOnTester.Designer.cs

--- PinscapeConfigTool/TvOnTester.Designer.cs
+++ PinscapeConfigTool/TvOnTester.Designer.cs
@@ -54,9 +54,9 @@
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(12, 16);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.Size = new System.Drawing.Size(111, 16);
             this.label5.TabIndex = 23;
-            this.label5.Text = "TV ON Tester";
+            this.label5.Text = "Testeur TV ON";
             // 
             // label4
             // 
@@ -77,29 +77,29 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(57, 92);
+            this.label2.Location = new System.Drawing.Point(4, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.Size = new System.Drawing.Size(151, 13);
             this.label2.TabIndex = 24;
-            this.label2.Text = "Power status pin:";
+            this.label2.Text = "Broche d\'état de l\'alimentation:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(91, 115);
+            this.label3.Location = new System.Drawing.Point(35, 115);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.Size = new System.Drawing.Size(120, 13);
             this.label3.TabIndex = 25;
-            this.label3.Text = "Latch pin:";
+            this.label3.Text = "Goupille de verrouillage:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(79, 138);
+            this.label6.Location = new System.Drawing.Point(52, 138);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.Size = new System.Drawing.Size(103, 13);
             this.label6.TabIndex = 26;
-            this.label6.Text = "TV relay pin:";
+            this.label6.Text = "Broche de relais TV:";
             // 
             // ckRelayOn
             // 
@@ -113,7 +113,7 @@
             this.ckRelayOn.Name = "ckRelayOn";
             this.ckRelayOn.Size = new System.Drawing.Size(92, 26);
             this.ckRelayOn.TabIndex = 27;
-            this.ckRelayOn.Text = "Relay On";
+            this.ckRelayOn.Text = "Relayer sur";
             this.ckRelayOn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.ckRelayOn.UseVisualStyleBackColor = true;
             this.ckRelayOn.CheckedChanged += new System.EventHandler(this.ckRelayOn_CheckedChanged);
@@ -132,7 +132,7 @@
             this.btnHelp.Name = "btnHelp";
             this.btnHelp.Size = new System.Drawing.Size(84, 27);
             this.btnHelp.TabIndex = 29;
-            this.btnHelp.Text = "Help";
+            this.btnHelp.Text = "Aide";
             this.btnHelp.UseVisualStyleBackColor = false;
             this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
             // 
@@ -150,7 +150,7 @@
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(91, 27);
             this.btnClose.TabIndex = 28;
-            this.btnClose.Text = "Close";
+            this.btnClose.Text = "Fermer";
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
@@ -169,11 +169,11 @@
             this.btnRelayPulse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRelayPulse.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRelayPulse.ForeColor = System.Drawing.Color.Purple;
-            this.btnRelayPulse.Location = new System.Drawing.Point(381, 128);
+            this.btnRelayPulse.Location = new System.Drawing.Point(338, 128);
             this.btnRelayPulse.Name = "btnRelayPulse";
-            this.btnRelayPulse.Size = new System.Drawing.Size(92, 26);
+            this.btnRelayPulse.Size = new System.Drawing.Size(135, 26);
             this.btnRelayPulse.TabIndex = 31;
-            this.btnRelayPulse.Text = "Pulse Relay";
+            this.btnRelayPulse.Text = "Relais d\'impulsions";
             this.btnRelayPulse.UseVisualStyleBackColor = true;
             this.btnRelayPulse.Click += new System.EventHandler(this.btnRelayPulse_Click);
             // 
@@ -221,11 +221,11 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(105, 161);
+            this.label9.Location = new System.Drawing.Point(129, 161);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.Size = new System.Drawing.Size(26, 13);
             this.label9.TabIndex = 35;
-            this.label9.Text = "Status:";
+            this.label9.Text = "Etat";
             // 
             // TvOnTester
             // 
@@ -252,7 +252,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "TvOnTester";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "TV ON Tester";
+            this.Text = "Testeur TV ON";
             this.Load += new System.EventHandler(this.TvOnTester_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
